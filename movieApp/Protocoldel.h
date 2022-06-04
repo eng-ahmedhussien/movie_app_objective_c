4918a3695e18755a39f37fb99cd09f0f7674d165
eng <33827384+eng-ahmedhussien@users.noreply.github.com>
2022-06-04 16:25:07 +0200

    gitignore

---
+++ movieApp/Protocoldel.h
@@ -0,0 +1,12 @@
+//
+//  Protocoldel.h
+//  movieApp
+//
+//  Created by Ahmed Hussien on 03/11/1443 AH.
+//
+
+#ifndef Protocoldel_h
+#define Protocoldel_h
+
+
+#endif /* Protocoldel_h */
---
+++ movieApp/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TableViewController.h
+//  movieApp
+//
+//  Created by Ahmed Hussien on 02/11/1443 AH.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
